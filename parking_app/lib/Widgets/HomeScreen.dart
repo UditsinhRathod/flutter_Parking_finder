53eaccc5c10a0045c63d1b896f21e6dff712f36b
Uditsinh_Rathod <rathoduditsinh03@gmail.com>
2026-05-20 15:10:17 +0530

    Final Commit

--- parking_app/lib/Widgets/HomeScreen.dart
+++ parking_app/lib/Widgets/HomeScreen.dart
@@ -3,27 +3,14 @@ import 'package:parking_app/Class.dart';
 import 'package:parking_app/Widgets/ParkingCard.dart';
 
 class Homescreen extends StatefulWidget {
-  final List<ParkingArea> parkingAreas;
-  const Homescreen({super.key, required this.parkingAreas});
+  List<ParkingArea> parkingAreas;
+  Homescreen({super.key, required this.parkingAreas});
 
   @override
   State<Homescreen> createState() => _HomescreenState();
 }
 
 class _HomescreenState extends State<Homescreen> {
-  void addParkingArea() {
-    setState(() {
-      widget.parkingAreas.add(
-        ParkingArea(
-          name: 'New Parking Area',
-          location: 'New Location',
-          city: 'New City',
-          totalSlots: 10,
-        ),
-      );
-    });
-  }
-
   String searchQuery = '';
 
   @override
@@ -34,102 +21,97 @@ class _HomescreenState extends State<Homescreen> {
           area.city.toLowerCase().contains(searchQuery.toLowerCase());
     }).toList();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: ListView.builder(
-          itemCount: filteredParkingAreas.length + 1,
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(20),
-                    width: 120,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey, width: 2),
-                      borderRadius: BorderRadius.circular(20),
+    return ListView.builder(
+      itemCount: filteredParkingAreas.length + 1,
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                width: 120,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 2),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Icon(Icons.search_outlined),
                     ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Icon(Icons.search_outlined),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            onChanged: (value) {
-                              setState(() {
-                                searchQuery = value;
-                              });
-                            },
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Search',
-                              contentPadding: EdgeInsets.symmetric(horizontal: 8),
-                              isDense: true,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.4),
-                          blurRadius: 10.0,
-                          offset: const Offset(4, 5),
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    margin: const EdgeInsets.all(10.0),
-                    child: ElevatedButton.icon(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Color(0xFF1565C0),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 12,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            searchQuery = value;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search',
+                          contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                          isDense: true,
                         ),
                       ),
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          "/AddParkingArea",
-                          arguments: {'toAdd': widget.parkingAreas, 'toEdit': null},
-                        ).then((_) {
-                          setState(() {});
-                        });
-                      },
-                      icon: Icon(Icons.add),
-                      label: Text('Add Parking Area'),
-                      autofocus: true,
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.4),
+                      blurRadius: 10.0,
+                      offset: const Offset(4, 5),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                margin: const EdgeInsets.all(10.0),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Color(0xFF1565C0),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
-                ],
-              );
-            }
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      "/AddParkingArea",
+                      arguments: {'toAdd': widget.parkingAreas, 'toEdit': null},
+                    ).then((_) {
+                      setState(() {});
+                    });
+                  },
+                  icon: Icon(Icons.add),
+                  label: Text('Add Parking Area'),
+                  autofocus: true,
+                ),
+              ),
+            ],
+          );
+        }
 
-            final parkingArea = filteredParkingAreas[index - 1];
-            return ParkingCard(
-              parkingArea: parkingArea,
-              getParkingAreas: widget.parkingAreas,
-              onDeleteCallback: () {
-                setState(() {
-                  widget.parkingAreas.remove(parkingArea);
-                });
-              },
-            );
+        final parkingArea = filteredParkingAreas[index - 1];
+        return ParkingCard(
+          parkingArea: parkingArea,
+          getParkingAreas: widget.parkingAreas,
+          onDeleteCallback: () {
+            setState(() {
+              widget.parkingAreas.remove(parkingArea);
+            });
           },
-      ),
+        );
+      },
     );
   }
 }
